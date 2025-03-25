bcc1e42e099315b3866ba95efb6dde3d66e4822a
TrungM8 <trungm8fordev@gmail.com>
2025-03-26 01:16:19 +0700

    modify profile image

--- src/app/components/ui/Infomation.tsx
+++ src/app/components/ui/Infomation.tsx
@@ -6,24 +6,28 @@ const Infomation = () => {
   return (
     <div
       id="info"
-      className="w-screen md:w-full md:h-[500px] bg-white overflow-hidden mt-10 md:mt-[100px]"
+      className="w-screen md:w-full md:h-[600px] bg-white overflow-hidden mt-10 md:mt-[100px]"
     >
-      <div className="md:px-20 px-10 pt-2 md:flex gap-10">
+      <div className="md:px-20 px-10 pt-2 md:flex gap-10  pb-20">
         <div className="mt-10">
-          <div className="w-[50px] h-[50px] overflow-hidden rounded-full border">
-            <Image
-              src="/avatar.jpg"
-              alt="Avatar Trung DelVin"
-              className="object-cover"
-              width={50}
-              height={50}
-            />
-          </div>
-          <div className="mt-10">
-            <h1 className="text-2xl font-extrabold md:text-3xl md:ml-10">
+          <div className="flex items-center">
+            <div className="w-[50px] h-[50px] overflow-hidden rounded-full border">
+              <Image
+                src="/avatar.jpg"
+                alt="Avatar Trung DelVin"
+                className="object-cover"
+                width={50}
+                height={50}
+              />
+            </div>
+            <h1 className="hidden text-2xl font-extrabold md:text-3xl md:ml-10 md:block">
               <i>"Uy tín mất một lần, khó khôi phục một đời"</i>
             </h1>
-
+          </div>
+          <div className="mt-10">
+            <h1 className="block text-2xl font-extrabold md:text-3xl md:hidden md:ml-10">
+              <i>"Uy tín mất một lần, khó khôi phục một đời"</i>
+            </h1>
             <p className="max-w-[400px] mt-5 md:ml-10 text-lg">
               Châm ngôn sống này mình cực kỳ tâm đắc, khách hàng không đễ kiếm
               được, mình luôn trân trọng
@@ -39,9 +43,9 @@ const Infomation = () => {
           </div>
           <Contact />
         </div>
-        <div className="hidden rounded-lg md:block">
+        <div className="hidden rounded-lg md:block border-2 border-gray-600 w-[500px] h-[500px] mt-10">
           <Image
-            src="/profile.jpg"
+            src="/profile.png"
             alt="Trung DelVin"
             width={500}
             height={500}
